c0df30de65c909496c51f50c7da3837fc6b3c878
Oluro Olaoluwa <ollaollu@gmail.com>
2017-06-30 14:38:21 +0100

    domain name fix

--- api/policies/enforceSsl.js
+++ api/policies/enforceSsl.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next) {
   var host = req.get('Host');
 
   if (host === 'zikoko-live.herokuapp.com') {
-    return res.redirect(301, ['http://', host, req.url].join(''));
+    return res.redirect(301, ['http://', 'zikoko.com', req.url].join(''));
   }
 
   if ((req.headers['x-forwarded-proto'] === 'https') && (process.env.NODE_ENV === 'production')) {
